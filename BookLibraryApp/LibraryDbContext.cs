4a948dc79ec2f08292222cdce192e261aac92604
tumusime_david <tumusimedavid14@gmail.com>
2024-09-20 09:34:55 +0200

    About implementing APIs

---
+++ BookLibraryApp/LibraryDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary;
+using System.Data.Entity;
+
+namespace BookLibraryApp
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext() : base("name=LibraryDbContext") // Connection string name
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {  modelBuilder.Entity<Book>()
+                .HasRequired(b => b.Author)   
+                .WithMany(a => a.Books)       
+                .HasForeignKey(b => b.AuthorId);
+
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
